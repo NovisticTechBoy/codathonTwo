c1c2ba6c24ab4249a9330d107423eadf6e24b1f7
NovisticTechBoy <154730483+NovisticTechBoy@users.noreply.github.com>
2025-02-03 11:24:02 -0800

    Add files via upload
    presence of optional (eye-slash) code in the Javascript file, incase of the need for an object to keep the password hidden
    presence of (optional) media-query code to handle the appearance of the form in any environment

---
+++ brinkWork.js
@@ -0,0 +1,20 @@
+
+    /*document.addEventListener("DOMContentLoaded", function () {
+        var passwordInput = document.getElementById("textAreaTwo");
+        var toggleIcon = document.getElementById("togglePassword");
+
+        if (toggleIcon && passwordInput) {
+            toggleIcon.addEventListener("click", function () {
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                    toggleIcon.classList.remove("fa-eye");
+                    toggleIcon.classList.add("fa-eye-slash");
+                } else {
+                    passwordInput.type = "password";
+                    toggleIcon.classList.remove("fa-eye-slash");
+                    toggleIcon.classList.add("fa-eye");
+                }
+            });
+        }
+    });
+*/
